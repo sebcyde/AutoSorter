31b71dfda04d3f9d1c6b4df61e2cbfefa4e04529
Sebastian <sebcyde@hotmail.com>
2023-09-28 13:25:20 +0100

    fixed pathing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // use crate::functions::append_log::append_log::append_log;
 use crate::functions::update_log::update_log;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub mod functions {
     pub mod append_log;
@@ -14,39 +14,40 @@ pub mod functions {
 fn main() {
     println!("Starting AutoSorter...");
 
+    let base_path: &Path;
+
     let is_at_work: bool = Path::new("C:/Users/sebastian.cyde").exists();
 
-    let _base_path: &Path;
-    let _logs_path: &Path;
-
     if is_at_work {
-        _logs_path = Path::new("C:/Users/sebastian.cyde/Documents/AutoSorter/Logs");
-        _base_path = Path::new("C:/Users/sebastian.cyde/Documents/AutoSorter");
+        base_path = Path::new("C:/Users/sebastian.cyde/Documents/AutoSorter");
     } else {
-        _logs_path = Path::new("C:/Users/SebCy/Documents/AutoSorter/Logs");
-        _base_path = Path::new("C:/Users/SebCy/Documents/AutoSorter");
+        base_path = Path::new("C:/Users/SebCy/Documents/AutoSorter");
     }
 
-    println!("Base path: {:?}", _base_path);
-    println!("Logs path: {:?}", _logs_path);
+    let logs_ext: PathBuf = Path::new("logs").to_path_buf();
+    let binding: PathBuf = Path::new(base_path).join(&logs_ext);
+    let logs_path: &Path = binding.as_path();
+
+    println!("Base path: {:?}", base_path);
+    println!("Logs path: {:?}", logs_path);
     println!(" ");
 
     // Create main AutoSorter directory if it doesnt exist
-    if !Path::new(_base_path).exists() {
+    if !Path::new(base_path).exists() {
         println!("No AutoSorter directory found. Creating...");
-        let _ = fs::create_dir_all(_base_path);
+        let _ = fs::create_dir_all(base_path);
         println!("Created successfully");
         println!(" ");
     };
 
     // Create directory for logs and initial log file
-    if !Path::new(&_logs_path).exists() {
+    if !Path::new(&logs_path).exists() {
         println!("No logs directory found. Creating...");
-        let _ = fs::create_dir_all(_logs_path);
+        let _ = fs::create_dir_all(logs_path);
         println!("Created successfully");
         println!(" ");
     };
 
-    let _ = update_log::update_log(_logs_path);
-    let _ = update_log::append_log("Example text to append", _logs_path);
+    let _ = update_log::update_log(logs_path);
+    let _ = update_log::append_log("Example text to append", logs_path);
 }
