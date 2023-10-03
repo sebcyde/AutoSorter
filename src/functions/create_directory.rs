1b805a55c2fa55e079df8c4dc9646c1ce419f2ad
Sebastian Cyde <sebastian.cyde@red-engine.com>
2023-10-03 17:45:07 +0100

    started file mover functions

--- src/functions/create_directory.rs
+++ src/functions/create_directory.rs
@@ -1,5 +1,26 @@
 pub mod create_dir {
+    use std;
+    use std::path::{Path, PathBuf};
+
     use tokio::fs;
+
+    pub fn create_containers(base_path: &Path) {
+        let extensions: Vec<&str> = vec![
+            "EXE", "JPG", "VIDEO", "PDF", "CSV", "JS", "GZ", "DB", "PHP", "PPTX", "ZIP", "EXCEL",
+            "AI", "TXT", "OTHER",
+        ];
+
+        for ext in extensions {
+            let ext_pathbuf: PathBuf = Path::new(ext).to_path_buf();
+            let binding: PathBuf = Path::new(base_path).join("Files").join(&ext_pathbuf);
+            let ext_path: &Path = binding.as_path();
+            if !Path::new(ext_path).exists() {
+                println!("Creating: {:?}", ext_path);
+                std::fs::create_dir_all(ext_path).expect("Failed to create directory");
+                println!(" ");
+            }
+        }
+    }
 
     pub async fn create_dir(new_dir_path: &str) -> String {
         match fs::metadata(new_dir_path).await {
